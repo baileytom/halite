9d779c4931e3ad1167b858cf709c05c6c4047316
Thomas Bailey <baileytb7@gmail.com>
2019-01-24 15:04:35 -0500

    Policy update v1 ready to test

--- policy_update.py
+++ policy_update.py
@@ -18,6 +18,7 @@ from collections import namedtuple
 import ast
 
 model_path = 'model/state'
+data_path = 'data/batch_data'
 
 # Model
 class PolicyNet(nn.Module):
@@ -55,10 +56,11 @@ policy_net.load_state_dict(torch.load(model_path))
 # Preprocessing
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 
-actions = []
-rewards = []
+savedactions = []
+savedrewards = []
+turns = []
 
-f = open("data/batch_data", "r")
+f = open(data_path, "r")
 for line in f.readlines():
     data = line.strip().split("|")
     t, reward, sample, state = data
@@ -75,17 +77,47 @@ for line in f.readlines():
     probs = policy_net(state)
     m = Categorical(probs)
     log_prob = m.log_prob(sample)
-        
+
+    # Log turn t
+    turns.append(t)
+    
     # Save reward for turn t
-    rewards.append(reward)
+    savedrewards.append(reward)
 
     # Save action for turn t
     action = SavedAction(log_prob, policy_net.get_state_value(state))
-    actions.append(action)
+    savedactions.append(action)
     
 # Policy updating
-print(actions)
-print(rewards)
-    
+#print(turns)
+#print(actions)
+#print(rewards)
 
+R = 0
+policy_losses = []
+value_losses = []
+rewards = []
+gamma = 0.99
 
+last_t = -1
+for (t, r) in zip(turns, savedrewards[::-1]):
+    # Check for new episode
+    if t < last_t:
+        R = 0
+    last_t = t
+    # Running reward
+    R = r + gamma * R
+    rewards.insert(0, R)
+rewards = torch.tensor(rewards)
+rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
+for (log_prob, value), r in zip(savedactions, rewards):
+    reward = r - value.item()
+    policy_losses.append(-log_prob * reward)
+    value_losses.append(F.smooth_l1_loss(value, torch.tensor([r])))
+optimizer.zero_grad()
+loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
+loss.backward()
+optimizer.step()
+
+# Save the updated model
+torch.save(policy_net.state_dict(), model_path)
